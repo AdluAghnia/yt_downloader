9f4103c943cda13b6e06a6c8cc1905bf579386dd
AdluAghnia <adluaghnia03@gmail.com>
2024-01-16 19:48:40 +0700

    FIXING STUFF

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
-
+from flask import Flask, send_file, render_template, url_for, redirect, request
+from download import download_video
+from pytube import YouTube
+from pathlib import Path
+import re
+import os
 app = Flask(__name__)
-
 
 @app.route("/")
 def index():
@@ -11,6 +14,41 @@ def index():
 
     return render_template("index.html")
 
+
+@app.route("/download", methods = ['GET', 'POST'])
+def download():
+    """
+    Fungsi ini untuk mendownload video dan memasukannya ke static file
+    """
+    message = ''
+    error_type = 0
+    if request.method == 'POST' and 'video_url' in request.form:
+        youtubeUrl = request.form["video_url"]
+        if youtubeUrl:
+            validateVideoUrl = (
+                r'(https?://)?(www\.)?'
+                '(youtube|youtu|youtube-nocookie)\.(com|be)/'
+                '(watch\?v=|embed/|v/|.+\?v=)?([^&=%\?]{11})'
+            )
+
+            validateVideoUrl = re.match(validateVideoUrl, youtubeUrl)
+
+            if validateVideoUrl:
+                url = YouTube(youtubeUrl)
+                video = url.streams.get_highest_resolution()
+                download_folder = str(os.path.join(Path.home(), "Downloads"))
+
+                video.download(download_folder)
+                message = "Video Telah Berhasil Di Download"
+                error_type = 1
+            else:
+                message = "Masukan Link Yang Valid"
+                error_type = 0
+        else:
+            message = "Masukakn Link Anda"
+            error_type = 0
+
+        return render_template('index.html', error_type=error_type, message = message)
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
 
--- download.py
+++ download.py
@@ -1,19 +1,15 @@
 from pytube import YouTube
 
-
-def get_title(yt):
-    return yt.title
-
-
-def download_video(yt):
-    yt.streams.filter(file_extension='mp4')
-    stream = yt.streams.get_by_itag(22)
-    stream.download()
-
+def download_video(yt, save_path : str, url : str):
+    """
+    Fungsi ini untuk mendownload video dan memasukannya ke static file
+    """
+    try:
+        yt.streams.filter(file_extension='mp4')
+        stream = yt.streams.get_by_itag(22)
+        stream.download(output_path=save_path)
+    except Exception as e:
+        print(e)
 if __name__ == "__main__":
-    link = str(input("Insert Link  : "))
-    yt = YouTube(link)
-
-    print(get_title(yt))
-    download_video(yt)
-    
+    download_video("./statics/vid_downloaded", "https://youtu.be/_xXGj-h4nto?si=AfnvKC-1ZC75Eb5D")
+   
